944025b99170fb9af3db113632317a40af44bd89
kingraham228 <kate.ingraham@knights.ucf.edu>
2021-06-02 14:38:59 -0400

    Initial commit

---
+++ src/main/java/oop/example/App.java
@@ -0,0 +1,12 @@
+package oop.example;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Person user = new Person();
+        user.calcBMI();
+
+    }
+}
---
+++ src/main/java/oop/example/Person.java
@@ -0,0 +1,66 @@
+package oop.example;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class Person {
+    private double height;
+    private double weight;
+
+    public double getHeight() {
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Enter your height in inches: ");
+            while (!input.hasNextDouble()) {
+                String str1 = input.next();
+                System.out.println(str1 + " is not a valid height.");
+                System.out.println("Enter your height in inches: ");
+            }
+            height = input.nextDouble();
+            if (height < 0) {
+                System.out.println("Height cannot be a negative value.");
+            }
+        } while (height < 0);
+        return height;
+    }
+
+    public double getWeight() {
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println("Enter your weight in pounds: ");
+            while (!input.hasNextDouble()) {
+                String str1 = input.next();
+                System.out.println(str1 + " is not a valid weight.");
+                System.out.println("Enter your weight in pounds: ");
+            }
+            weight = input.nextDouble();
+            if (weight < 0) {
+                System.out.println("Weight cannot be a negative value.");
+            }
+        } while (weight < 0);
+        return weight;
+    }
+
+    public void calcBMI(){
+        height = getHeight();
+        weight = getWeight();
+        double step1 = height * height;
+        double step2 = weight / step1;
+        double bmi = step2 * 703;
+        BigDecimal roundBMI = BigDecimal.valueOf(bmi);
+        roundBMI = roundBMI.setScale(2, RoundingMode.HALF_UP);
+        bmi = roundBMI.doubleValue();
+
+        System.out.println("Your BMI is "+bmi+".");
+
+        if(bmi < 18.5){
+            System.out.println("You are underweight. You should see your doctor.");
+        } else if(bmi > 25){
+            System.out.println("You are overweight. You should see your doctor.");
+        }else{
+            System.out.println("You are within the ideal weight range.");
+        }
+
+    }
+}
